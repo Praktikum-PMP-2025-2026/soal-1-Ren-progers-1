2b0dcabea95803c9f76b1563d202604588b83321
Ren-progers <kidunggamel@gmail.com>
2026-04-20 09:07:52 +0700

    Update soal1.c
    commit4

--- soal1.c
+++ soal1.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 #include <stdio.h>
 
 int main(void) {
@@ -11,13 +10,14 @@ int main(void) {
     }
     for (int i = 0 ; i < N; i++){
         int sumbuff = 0;
-        int valid = 0;
+        int valid = 0, lr = 0;
         if (arr[i] == -1){
             int k = i-1;
             while (valid == 0 && k >= 0){
                 if (arr[k] != -1){
                     sumbuff += arr[k];
                     valid = 1;
+                    lr++;
                 }
                 k--;
             }
@@ -27,19 +27,29 @@ int main(void) {
                 if (arr[j] != -1){
                     sumbuff += arr[j];
                     valid = 1;
+                    lr++;
                 }
                 j++;
             }
-            
-            arr[i] = sumbuff/2;
+            if (lr != 2){
+                arr [i] = sumbuff;
+            }
+            else {
+                arr[i] = sumbuff/2;
+            }
         }
     }
     printf ("RECOVERED ");
     for (int i = 0; i < N ; i++){
-        printf ("%d ", arr[i]);
+        if (i == N-1){
+            printf ("%d", arr[i]);
+        }
+        else {
+            printf ("%d ", arr[i]);
+        }
         sum += arr[i];
     }
     printf ("\n");
-    printf ("MAX_SUM %d\n", sum);
+    printf ("MAX_SUM %d", sum);
     return 0;
 }
