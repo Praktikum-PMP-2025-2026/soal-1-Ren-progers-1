d71724a24a950585b57316f55fb5bb4db45a5a96
Ren-progers <kidunggamel@gmail.com>
2026-04-20 08:54:22 +0700

    Update soal1.c
    upfirst

--- soal1.c
+++ soal1.c
@@ -1,5 +1,43 @@
 #include <stdio.h>
 
 int main(void) {
+    int N, sum = 0;
+    printf ("hellyeah\n");
+    scanf ("%d", &N);
+    int arr[N];
+    for (int i = 0; i < N; i++){
+        scanf ("%d", &arr[i]);
+    }
+    for (int i = 0 ; i < N; i++){
+        int sumbuff = 0;
+        int valid = 0;
+        if (arr[i] == -1){
+            int k = i-1;
+            while (valid == 0 && k >= 0){
+                if (arr[k] != -1){
+                    sumbuff += arr[k];
+                }
+                k--;
+            }
+            valid = 0;
+            int j = i+1;
+            while (valid == 0 && j < N){
+                if (arr[j] != -1){
+                    sumbuff += arr[j];
+                    valid = 1;
+                }
+                j++;
+            }
+            
+            arr[i] = sumbuff/2;
+        }
+    }
+    printf ("RECOVERED ");
+    for (int i = 0; i < N ; i++){
+        printf ("%d ", arr[i]);
+        sum += arr[i];
+    }
+    printf ("\n");
+    printf ("MAX SUM : %d\n", sum);
     return 0;
 }
